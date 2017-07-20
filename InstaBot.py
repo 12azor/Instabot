65ee7c93695760837a9b2c95ad1124c83f26eb14
ankit <ankit.uniyal29@gmail.com>
2017-07-20 22:55:31 +0530

    Creation of Token and base Url

---
+++ InstaBot.py
@@ -0,0 +1,7 @@
+import requests
+import urllib
+
+my_token=5761930245.c3f4cf5.2dcccf47685c424e94cfb140c9f3dab9
+base_url="https://api.instagram.com/v1/"
+
+user_name=raw_input("Enter the Username fo which you want to peform further actions: ")
