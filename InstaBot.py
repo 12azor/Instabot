2364ed1b58241714f645b1c4f666cfbe564f2800
ankit <ankit.uniyal29@gmail.com>
2017-07-21 17:05:09 +0530

    Creation of futher new fuctions and menu and debugging 3

--- InstaBot.py
+++ InstaBot.py
@@ -6,28 +6,38 @@ my_token="1606815543.feefa28.c2601ebc8ed5493481c5e0b5f72ec84e"
 base_url="https://api.instagram.com/v1/"
 
 #--------------------Function to get Self Information--------------------#
+
 def self_info():
     url=base_url+"users/self/?access_token=%s"%(my_token)
     result= requests.get(url).json()
     print(json.dumps(result, indent=3))
+
+    
     
     
 #--------------------Function to get information of user from provided username--------------------#
+    
 def get_user_info(user_id):
     url=base_url + ("users/%s/?access_token=%s") % (user_id, my_token)
     result=requests.get(url).json()
     print(json.dumps(result, indent=3))
+
+    
     
 
 #--------------------Function to get recent media liked by self--------------------#
+    
 def recent_media_liked():
     url=base_url+("users/self/media/liked?access_token=%s") % (my_token)
     result=requests.get(url).json()    
     print "The recent media liked by the user is : "
     print (json.dumps(result["data"][0],indent= 3))
+
+    
     
 
 #--------------------Function to return USER ID--------------------#
+    
 def get_user_id(user_name):
     url = (base_url + "users/search?q=%s&access_token=%s") % (user_name, my_token)
     result=requests.get(url).json()
@@ -36,8 +46,11 @@ def get_user_id(user_name):
     else:
         return 0
 
+    
+
         
 #--------------------Function to fetch info of self recent post--------------------#
+    
 def get_own_post():
     url=base_url+"users/self/media/recent/?access_token=%s" %(my_token)
     result= requests.get(url).json()
@@ -51,8 +64,11 @@ def get_own_post():
     else:
         return 0
 
+    
+
 
 #--------------------Function to Check if user exixts and has some Information--------------------#
+    
 def search_user(user_name):
     url = (base_url + "users/search?q=%s&access_token=%s") % (user_name, my_token)
     result=requests.get(url).json()
@@ -62,10 +78,13 @@ def search_user(user_name):
     else:
         print "Either the Username does not exist in sandbox OR does not contain any information to retrive."
         return(0)
+
+
     
 
 
 #--------------------Function to get User's post--------------------#
+    
 def get_user_post(username):
     user_id = get_user_id(username)
     if user_id == None:
@@ -75,7 +94,7 @@ def get_user_post(username):
         user_media = requests.get(url).json()
         if user_media['meta']['code'] == 200:
             if len(user_media['data']):
-                option=int(raw_input("You want to choose the post on the basis of:\n1. Most Recent\n2. Max Likes\n3. Max Comments\nOption: "))
+                option=int(raw_input("You want to choose the post on the basis of:\n1. Most Recent\n2. Max Likes\n3. Max Comments\n4. Least Comments.\nOption: "))
                                      
                 if option==1:
                     return user_media['data'][0]['id']
@@ -101,6 +120,17 @@ def get_user_post(username):
                             media_id=max_like["id"]
                         counter=counter+1
                     return media_id
+
+                elif option==4:
+                    counter=1
+                    media_id=user_media["data"][0]["id"]
+                    max_like=user_media["data"][0]
+                    while len(user_media["data"])>counter:
+                        if max_like["comments"]["count"]>user_media["data"][counter]["comments"]["count"]:
+                            max_like=user_media["data"][counter]
+                            media_id=max_like["id"]
+                        counter=counter+1
+                    return media_id
                               
             else:
                 print "There is no post!"
@@ -108,7 +138,34 @@ def get_user_post(username):
             print "Status code other than 200 received!"
 
 
+            
+
+
+#--------------------Function to print media comments of a user--------------------#
+            
+def view_media_comments(media_id):
+    url=base_url+("media/%s/comments?access_token=%s")%(media_id,my_token)
+    result=requests.get(url).json()
+    if len(result["data"])==0:
+        print "\nThere is no comment on this post.\nMedia id is: %s" %(media_id)
+    else:
+        c=int(raw_input("If you want to see detailed info with comment press 1 else any other number for just comments: \n"))
+        if c==1:
+            print (json.dumps(result["data"], indent=3))
+        else:
+            comments=[]
+            for i in range(0,len(result["data"])):
+                comments.append(result["data"][i]["text"])
+            for i in comments:
+                print i
+        print "\nComments successfully Printed\n"
+
+
+
+
+
 #--------------------Function to download self recent post--------------------#
+        
 def self_download_post():
     url=base_url+"users/self/media/recent/?access_token=%s" %(my_token)
     result= requests.get(url).json()
@@ -118,7 +175,11 @@ def self_download_post():
     print "Your image has been downloaded!"
 
 
+
+
+
 #--------------------Function to download USER's recent post--------------------#
+    
 def user_download_post(user_name,media_id):
     user_id = get_user_id(user_name)
     if user_id != None:
@@ -137,6 +198,42 @@ def user_download_post(user_name,media_id):
         print "User Doesnot Exist"
 
 
+
+#--------------------Function to Comment on a post of user--------------------#
+
+def make_a_comment(user_name):
+    media_id=get_user_post(user_name)
+    comment=raw_input("Enter the comment you want to post: ")
+    payload = {"access_token": my_token, "text" : comment}
+    request_url = (base_url + "media/%s/comments") % (media_id)
+    make_comment = requests.post(request_url, payload).json()
+    if make_comment['meta']['code'] == 200:
+        print "\nSuccessfully added the comment!"
+    else:
+        print "\nUnable to add the comment. Please try again!"
+
+
+
+
+#--------------------Function to like a post of user--------------------#
+
+def like_a_post(user_name):
+    media_id = get_user_post(user_name)
+    url = (base_url + 'media/%s/likes') % (media_id)
+    payload = {"access_token": my_token}
+    post_a_like = requests.post(url, payload).json()
+    if post_a_like['meta']['code'] == 200:
+        print "\nThe post has been Liked"
+    else:
+        print "\nYour like was unsuccessful on the post. Please Try again!"
+    
+
+
+##################################################################################################################
+        
+#=============================================Main Program with menu=============================================#
+
+
 while True:
     user_option=int(raw_input("Enter the number corresponding to the options you want to perform the actions on:\n1. Self\n2. Other Users\n3. Exit.\nOption: "))
     
@@ -148,14 +245,17 @@ while True:
             
         elif choice_option==2:
             my_post_id=get_own_post()
+            
             if my_post_id==0:
                 print "Some other code other than 200 recieved."
             elif my_post_id != None:
                 print "My recent post's ID is: "+str(my_post_id)
                 print "The recent Image will now be downloaded...."
                 self_download_post()
+                
         elif choice_option==3:
             recent_media_liked()
+            
         else:
             print "You have entered a wrong option. Try again./n"
             
@@ -166,10 +266,11 @@ while True:
         validate_username=search_user(username)
         
         if validate_username:
-            username_choice=int(raw_input("Would you like to:\n1. Get %s 's user ID.\n2. Fetch %s 's POST.\n3. Print general Info of %s.\n4. Get comments on a post of %s.\n5. Make acomment on %s 's post.\n6. Like %s 's post.\nOption: " %(username,username,username,username,username,username)))
+            username_choice=int(raw_input("Would you like to:\n1. Get %s 's user ID.\n2. Fetch %s 's POST.\n3. Print general Info of %s.\n4. Get comments on a post of %s.\n5. Make a comment on %s 's post.\n6. Like %s 's post.\nOption: " %(username,username,username,username,username,username)))
 
             if username_choice==1:
                 user_id=get_user_id(username)
+                
                 if user_id==0:
                     print "Some Other code than 200 recieved."
                 else:
@@ -177,6 +278,7 @@ while True:
                     
             elif username_choice==2:
                 media_id=get_user_post(username)
+                
                 if media_id!= None:
                     print "The ID of recent media is: "+str(media_id)
                     print "The Image with above ID will now be downloaded....."
@@ -185,6 +287,26 @@ while True:
             elif username_choice==3:
                 user_id=get_user_id(username)
                 get_user_info(user_id)
+
+            elif username_choice==4:
+                media_id=get_user_post(username)
+                
+                if media_id!=None:
+                    view_media_comments(media_id)
+
+            elif username_choice==5:                
+                make_a_comment(username)
+
+            elif username_choice==6:
+                like_a_post(username)
+
+            else:
+                print "Wrong input, try again...."
+        continue
+    elif user_option==3:
+        print "\n--------------------------------------------------------------\nThe application will now exit...Thanks for using the INSTA-BOT"
+        exit()
+                
                 
                 
                 
